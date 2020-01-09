7c4b576fa05c298844335a6d957423245b896596
dwr <damian.wrobel.1990@gmail.com>
2020-01-09 19:49:21 +0100

    usuwanie kierowcy

---
+++ src/main/java/sdacademy/hibernate/demo/DeleteDriverDetailDemo.java
@@ -0,0 +1,49 @@
+package sdacademy.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import sdacademy.demo.entity.Driver;
+import sdacademy.demo.entity.DriverDetail;
+
+public class DeleteDriverDetailDemo {
+
+    public static void main(String[] args) {
+        //tworzenie fabryki
+        // dodanie konfiguracji, dodanie klas
+        SessionFactory factory = new Configuration().configure("hibernate.cfg.xml")
+                .addAnnotatedClass(DriverDetail.class)
+                .addAnnotatedClass(Driver.class)
+                .buildSessionFactory();
+        //tworzenie sesji na potrzeby naszej pracy
+        Session session = factory.getCurrentSession();
+
+        try {
+            //rozpocznij transakcję żeby zapisać
+            session.beginTransaction();
+
+            Long id = 6L;
+            DriverDetail driverDetail = session.get(DriverDetail.class, id);
+
+            System.out.println(driverDetail);
+
+            System.out.println("---------Kierowca pobrany ze szczegółów-----------");
+            System.out.println(driverDetail.getDriver());
+
+            //zakomituj transakcję
+
+            session.delete(driverDetail);
+
+            session.getTransaction().commit();
+
+        } catch (Exception e){
+            e.printStackTrace();
+
+        } finally {
+            session.close();
+            //posprzątaj po otwartej sesji
+            factory.close();
+        }
+    }
+
+}
